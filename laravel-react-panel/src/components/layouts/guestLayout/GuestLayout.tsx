e26883b15fe8b478b4d8a4311e74875ac816c414
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2024-01-06 20:26:26 +0300

    updated guestLayout to support new login & signup

--- laravel-react-panel/src/components/layouts/guestLayout/GuestLayout.tsx
+++ laravel-react-panel/src/components/layouts/guestLayout/GuestLayout.tsx
@@ -1,5 +1,32 @@
 import { Navigate, Outlet } from "react-router-dom"
 import { useStateContext } from "../../../contexts/ContextProdiver"
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 export default function GuestLayout() {
   const {token} = useStateContext();
@@ -7,11 +34,23 @@ export default function GuestLayout() {
   if(token) {
     return <Navigate to="/" />
   }
-
+  const defaultTheme = createTheme();
   return(
-    <div>
-      Put your
-      <Outlet />
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Outlet />
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
   )
 }
